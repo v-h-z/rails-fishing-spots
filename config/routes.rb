7f7dbca3054ee768a0b56a4228ca186abd2bdf4c
victor holl <v_h@me.com>
2020-08-13 15:51:31 +0200

    added index + show

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'spots/index'
-  get 'spots/new'
-  get 'spots/create'
   root to: 'pages#home'
   get "/spots", to: "spots#index"
+  get "/spots/:id", to: "spots#show", as: "spot"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
